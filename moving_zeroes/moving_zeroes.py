7a8078766368f59e6ba0e3e034dde3e65cb159ef
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-21 11:19:57 -0600

    Implement moving zeroes algorithm

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,27 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # Find the last nonzero number in the array
+    nonzero_index = len(arr) - 1
+    while arr[nonzero_index] == 0:
+        nonzero_index -= 1
+        # The entire right side of the array is zeroes,
+        # we don't need to do anything else
+        if nonzero_index == 0:
+            return arr
 
-    pass
-
+    for i in range(len(arr)):
+        if i >= nonzero_index:
+            break
+        if arr[i] == 0:
+            arr[i] = arr[nonzero_index]
+            arr[nonzero_index] = 0
+            while arr[nonzero_index] == 0:
+                nonzero_index -= 1
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 3, 0, 1, 0, -2]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
