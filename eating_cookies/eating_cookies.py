1b01dae882aa5381c27e561233a8de0f500da415
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-21 12:20:22 -0600

    Implement eating cookies with cache to speed up calculations

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
-
-    pass
+def eating_cookies(n, cache=None):
+    if cache and cache[n] != 0:
+        return cache[n]
+    if n <= 0:
+        return 1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    else:
+        if cache is None:
+            cache = [0] * (n + 1)
+        result = eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
+        cache[n] = result
+        return result
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
