ad19727d05902e4f49b4259e84ffab210b2a9819
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-21 12:36:06 -0600

    Implement stretch goal rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,11 +2,20 @@
 
 import sys
 
+POSSIBLE_MOVES = ["rock", "paper", "scissors"]
 def rock_paper_scissors(n):
-  # Your code here
-
-  pass
-
+    possible_plays = []
+    indices = [0] * n
+    while True:
+        possible_plays.append([POSSIBLE_MOVES[i] for i in indices])
+        for i in range(len(indices)-1, -2, -1):
+            if i == -1:
+                return possible_plays
+            indices[i] += 1
+            if indices[i] >= len(POSSIBLE_MOVES):
+                indices[i] = 0
+                continue
+            break
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
